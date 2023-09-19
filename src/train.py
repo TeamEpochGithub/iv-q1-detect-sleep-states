22549445f819f886f61c1f58a54965e998660154
JasperVS <jmvanselm@gmail.com>
2023-09-19 16:18:14 +0200

    Add initial wandb setup

--- src/train.py
+++ src/train.py
@@ -1,7 +1,33 @@
 # This file does the training of the model
 
+# Imports
+import wandb
+import random
+
 # Load config file
 config = None
 
 def train(config):
-    return
+    wandb.init(
+        project='detect-sleep-states',
+
+        config={
+            'name': 'setup_run',
+            'learning_rate': 0.01,
+            'epochs': 10,
+        }
+    )
+    # simulate training
+    epochs = 10
+    offset = random.random() / 5
+    for epoch in range(2, epochs):
+        acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+        loss = 2 ** -epoch + random.random() / epoch + offset
+        
+        # log metrics to wandb
+        wandb.log({"acc": acc, "loss": loss})
+        
+    # [optional] finish the wandb run, necessary in notebooks
+    wandb.finish()
+
+train(config)

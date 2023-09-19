da7b4e99bb4b2d7293e1b1e841903aff989600df
tolgakopar <cahittolgakopar@gmail.com>
2023-09-19 16:52:20 +0200

    fixed the stupid flake shit

--- src/dataframe_mem_reduce.py
+++ src/dataframe_mem_reduce.py
@@ -1,15 +1,19 @@
+import numpy as np
+import gc
+
+
 def reduce_mem_usage(df):
-    """ 
-    Iterate through all numeric columns of a dataframe and modify the data type
-    to reduce memory usage.        
     """
-    
-    start_mem = df.memory_usage().sum() / 1024**2
+    Iterate through all numeric columns of a dataframe and modify the data type
+    to reduce memory usage.
+    """
+
+    start_mem = df.memory_usage().sum() / 1024 ** 2
     print(f'Memory usage of dataframe is {start_mem:.2f} MB')
-    
+
     for col in df.columns:
         col_type = df[col].dtype
-        
+
         if col_type != object and col_type != 'datetime64[ns, UTC]':
             c_min = df[col].min()
             c_max = df[col].max()
@@ -21,7 +25,7 @@ def reduce_mem_usage(df):
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
+                    df[col] = df[col].astype(np.int64)
             else:
                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
@@ -30,10 +34,10 @@ def reduce_mem_usage(df):
                 else:
                     df[col] = df[col].astype(np.float64)
 
-    end_mem = df.memory_usage().sum() / 1024**2
+    end_mem = df.memory_usage().sum() / 1024 ** 2
     print(f'Memory usage after optimization is: {end_mem:.2f} MB')
     decrease = 100 * (start_mem - end_mem) / start_mem
     print(f'Decreased by {decrease:.2f}%')
     gc.collect()
-    
+
     return df

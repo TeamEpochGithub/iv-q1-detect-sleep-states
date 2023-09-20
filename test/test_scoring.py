742bb5538b79f90448d86d1145ef9b8e2227febc
tolgakopar <cahittolgakopar@gmail.com>
2023-09-20 10:12:17 +0200

    added 2 more tests to the unittest

--- test/test_scoring.py
+++ test/test_scoring.py
@@ -3,7 +3,7 @@ from src.scoring import score
 
 
 class Test(TestCase):
-    def test_score(self):
+    def test_score1(self):
         import pandas as pd
 
         column_names = {
@@ -30,3 +30,45 @@ class Test(TestCase):
                        plot_precision_recall=False)
 
         self.assertEqual(result, 0.6666666666666666)
+
+    def test_score2(self):
+        import pandas as pd
+        column_names = {
+            'series_id_column_name': 'video_id',
+            'time_column_name': 'time',
+            'event_column_name': 'event',
+            'score_column_name': 'score', }
+        tolerances = {'pass': [1.0]}
+        solution = pd.DataFrame({
+            'video_id': ['a', 'a'],
+            'event': ['pass', 'pass'],
+            'time': [0, 15], })
+        submission = pd.DataFrame({
+            'video_id': ['a', 'a', 'a'],
+            'event': ['pass', 'pass', 'pass'],
+            'score': [1.0, 0.5, 1.0],
+            'time': [0, 10, 14.5], })
+        result = score(solution, submission, tolerances, **column_names, plot_precision_recall=False)
+
+        self.assertEqual(result, 1.0)
+
+    def test_score3(self):
+        import pandas as pd
+        column_names = {
+            'series_id_column_name': 'video_id',
+            'time_column_name': 'time',
+            'event_column_name': 'event',
+            'score_column_name': 'score', }
+        tolerances = {'pass': [0.1, 0.2, 1.0]}
+        solution = pd.DataFrame({
+            'video_id': ['a', 'a'],
+            'event': ['pass', 'pass'],
+            'time': [0, 15], })
+        submission = pd.DataFrame({
+            'video_id': ['a', 'a', 'a'],
+            'event': ['pass', 'pass', 'pass'],
+            'score': [1.0, 0.5, 1.0],
+            'time': [0, 10, 14.5], })
+        result = score(solution, submission, tolerances, **column_names, plot_precision_recall=True)
+
+        self.assertEqual(result, 0.5)

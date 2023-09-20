c95800bd0a665320769708c974211ac324853d73
JasperVS <jmvanselm@gmail.com>
2023-09-20 11:12:29 +0200

    Create model class and init.py

---
+++ src/models/example_model.py
@@ -0,0 +1,14 @@
+# This is a sample model file. You can use this as a template for your own models.
+# The model file should contain a class that inherits from the Model class.
+from model import Model
+
+
+class ExampleModel(Model):
+    # Create different training function
+    def train(self, data):
+        # Train function
+        print("Training model")
+
+
+ex = ExampleModel()
+ex.train(1)

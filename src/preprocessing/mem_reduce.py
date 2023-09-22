9e4163a60ffaa45a6766c8fbb2f198fe0d3cc5dd
tolgakopar <cahittolgakopar@gmail.com>
2023-09-22 17:07:44 +0200

    cleaned up mem_reduce and finizhed save to parquet
    IMPORTANT: mem reduce doesnt let us save to parquet

--- src/preprocessing/mem_reduce.py
+++ src/preprocessing/mem_reduce.py
@@ -1,13 +1,51 @@
 # This class is to reduce memory usage of dataframe
 from src.preprocessing.pp import PP
-from src.preprocessing.dataframe_mem_reduce import reduce_mem_usage
+import numpy as np
+import gc
 
 
 class MemReduce(PP):
-    def __init__(self):
-        # Initiate class
-        pass
 
     def preprocess(self, df):
-        df = reduce_mem_usage(df)
+        df = self.reduce_mem_usage(df)
+        return df
+
+    def reduce_mem_usage(self, df):
+        """
+        Iterate through all numeric columns of a dataframe and modify the data type
+        to reduce memory usage.
+        """
+
+        start_mem = df.memory_usage().sum() / 1024 ** 2
+        print(f'Memory usage of dataframe is {start_mem:.2f} MB')
+
+        for col in df.columns:
+            col_type = df[col].dtype
+
+            if col_type != object and col_type != 'datetime64[ns, UTC]':
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+        end_mem = df.memory_usage().sum() / 1024 ** 2
+        print(f'Memory usage after optimization is: {end_mem:.2f} MB')
+        decrease = 100 * (start_mem - end_mem) / start_mem
+        print(f'Decreased by {decrease:.2f}%')
+        gc.collect()
+
         return df
--- src/preprocessing/pp.py
+++ src/preprocessing/pp.py
@@ -4,25 +4,25 @@ import os
 
 
 class PP:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        pass
 
     def preprocess(self, data):
         raise NotImplementedError
 
     def run(self, data, curr):
         # check if the prev path exists
-        path = '../data/processed/' + '_'.join(curr[:-1]) + '.parquet'
+        path = 'data/processed/' + '_'.join(curr) + '.parquet'
         if os.path.exists(path):
             print(f'Preprocessed data already exists, reading from {path}')
-            processed = pd.read_parquet(path)
+            processed = pd.read_csv(path)
             print(f'Data read from {path}')
         else:
             # recalculate the current path to save the data
-            path = '../data/processed/' + '_'.join(curr) + '.parquet'
+            path = 'data/processed/' + '_'.join(curr) + '.csv'
             print('Preprocessed data does not exist, applying preprocessing')
             processed = self.preprocess(data)
-            print('Preprocessing has been applied, ready to save the data')
+            print(f'Preprocessing has been applied, ready to save the data to {path}')
             if not isinstance(processed, pd.DataFrame):
                 raise TypeError('Preprocessing step did not return a pandas DataFrame')
             else:

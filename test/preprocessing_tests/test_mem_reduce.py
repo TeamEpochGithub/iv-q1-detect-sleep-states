a8cda6413a0b5524f2ad5322db45820360d63230
tolgakopar <cahittolgakopar@gmail.com>
2023-09-25 09:45:37 +0200

    test runs locally now

--- test/preprocessing_tests/test_mem_reduce.py
+++ test/preprocessing_tests/test_mem_reduce.py
@@ -7,15 +7,16 @@ from src.preprocessing.mem_reduce import MemReduce
 class Test(TestCase):
     def test_mem_reduce(self):
         # read the data
-        train_series = pd.read_parquet("test/train_series.parquet")
+        mem_reducer = MemReduce()
+        train_series = pd.read_parquet("test/test_series.parquet")
 
-        print('data usage of train_series before mem_reduce:', '\n')
+        print('data usage of test_series before mem_reduce:', '\n')
         series_mem_used_before = train_series.memory_usage().sum()
 
         # now do the mem_reduce
 
-        print('data usage of train_series after mem_reduce:', '\n')
-        series_mem_used_after = MemReduce.reduce_mem_usage(train_series).memory_usage().sum()
+        print('data usage of test_series after mem_reduce:', '\n')
+        series_mem_used_after = mem_reducer.reduce_mem_usage(train_series).memory_usage().sum()
 
         # assert that memory usage for both dataframes went down
         self.assertTrue(series_mem_used_before > series_mem_used_after)

5f38a1e281236331ab4433736095da43da42de0e
hjdeheer <hugodeheer1234@gmail.com>
2023-09-26 16:39:04 +0200

    Add loss class and optimizer class

--- src/loss/loss.py
+++ src/loss/loss.py
@@ -1,13 +1,36 @@
 # This is the base class for loss
+from torch import nn
+
+
+class LossException(Exception):
+    pass
+
 
 class Loss():
+    """
+    This is a static class for loss functions.
+    """
     def __init__(self):
         # Init function
         pass
 
-    def forward(self, y_pred, y_true):
-        # Forward function
-        pass
+    def get_loss(self, loss_name):
+        """
+        This function looks up the correct loss function and returns it.
+        :param loss_name: name of the loss function
+        :return: the loss function
+        """
+        match loss_name:
+            case "mse-torch":
+                return nn.MSELoss()
+            case "mae-torch":
+                return nn.L1Loss()
+            case "crossentropy-torch":
+                return nn.CrossEntropyLoss()
+            case "binarycrossentropy-torch":
+                return nn.BCELoss()
+            case _:
+                raise LossException("Loss function not found: " + loss_name)
 
-    def backward(self, y_pred, y_true):
-        raise NotImplementedError
+
+
---
+++ src/optimizer/optimizer.py
@@ -0,0 +1,39 @@
+# This is the base class for loss
+import torch
+import torch.optim as optim
+
+
+class OptimizerException(Exception):
+    pass
+
+
+class Optimizer():
+    """
+    This is a static class for loss functions.
+    """
+    def __init__(self):
+        # Init function
+        pass
+
+    def get_optimizer(self, optimizer_name, learning_rate, model):
+        """
+        This function looks up the correct loss function and returns it.
+        :param loss_name: name of the loss function
+        :return: the loss function
+        """
+        match optimizer_name:
+            case "adam-torch":
+                return optim.Adam(model.parameters(), lr=learning_rate)
+            case "sgd-torch":
+                return optim.SGD(model.parameters(), lr=learning_rate)
+            case "adagrad-torch":
+                return optim.Adagrad(model.parameters(), lr=learning_rate)
+            case "adadelta-torch":
+                return optim.Adadelta(model.parameters(), lr=learning_rate)
+            case "rmsprop-torch":
+                return optim.RMSprop(model.parameters(), lr=learning_rate)
+            case _:
+                raise OptimizerException("Optimizer function not found: " + optimizer_name)
+
+
+

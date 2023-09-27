5949f694b6c8d39df7df48074e9d510193b15799
tolgakopar <cahittolgakopar@gmail.com>
2023-09-27 10:20:02 +0200

    push changes to add_hour to add it after merging main

--- src/preprocessing/add_hour.py
+++ src/preprocessing/add_hour.py
@@ -8,8 +8,11 @@ class AddHour(PP):
         pass
 
     def preprocess(self, data):
+        # convert pandas dataframe to polars dataframe to make column operations faster
         if isinstance(data, pd.DataFrame):
             data = pl.from_pandas(data)
+        print(data.head())
+        # do the datetime operations on the polars dataframe
         data = data.with_columns(pl.col("timestamp").str.slice(11, 8).alias("time"))
         data = data.with_columns(pl.col("time").str.to_datetime(format="%H:%M:%S").cast(pl.Datetime))
         print(data.head())

b08f57c4e2b8437c01f206d116f2382b7acf95c5
tolgakopar <cahittolgakopar@gmail.com>
2023-09-27 09:47:20 +0200

    removed unused coments

--- src/preprocessing/add_hour.py
+++ src/preprocessing/add_hour.py
@@ -8,15 +8,11 @@ class AddHour(PP):
         pass
 
     def preprocess(self, data):
-        # data['timestamp'] = data['timestamp'].cast(pl.Datetime)
         if isinstance(data, pd.DataFrame):
             data = pl.from_pandas(data)
-        print('mark1')
         data = data.with_columns(pl.col("timestamp").str.slice(11, 8).alias("time"))
         data = data.with_columns(pl.col("time").str.to_datetime(format="%H:%M:%S").cast(pl.Datetime))
         print(data.head())
-        # data.with_columns(pl.col('timestamp').str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M:%S").cast(pl.Datetime))
-        print('mark2')
         # convert polars dataframe back to pandas dataframe
         data = data.to_pandas()
         return data

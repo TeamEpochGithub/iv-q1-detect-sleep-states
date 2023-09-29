e2e4a3ec05ebafa7cdd18e2783b07b388428d982
Emiel Witting <emielwitting@teamepoch.net>
2023-09-29 15:37:36 +0200

    fix memory usage unit test not printing

--- test/preprocessing_tests/test_mem_reduce.py
+++ test/preprocessing_tests/test_mem_reduce.py
@@ -10,13 +10,12 @@ class Test(TestCase):
         mem_reducer = MemReduce()
         train_series = pd.read_parquet("test/test_series.parquet")
 
-        print('data usage of test_series before mem_reduce:', '\n')
         series_mem_used_before = train_series.memory_usage().sum()
-
+        print('data usage of test_series before mem_reduce:', series_mem_used_before)
         # now do the mem_reduce
 
-        print('data usage of test_series after mem_reduce:', '\n')
         series_mem_used_after = mem_reducer.reduce_mem_usage(train_series).memory_usage().sum()
+        print('data usage of test_series after mem_reduce:', series_mem_used_after)
 
         # assert that memory usage for both dataframes went down
         self.assertTrue(series_mem_used_before > series_mem_used_after)

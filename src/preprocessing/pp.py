b9a3d6c97b5504002a7a9262a436e0dde6cab6f1
tolgakopar <cahittolgakopar@gmail.com>
2023-10-02 14:17:34 +0200

    optimized pp step a lot more
    Unintentionally made it so we dont loop over the preprocessing steps in main but inside the pp base class run method

--- src/preprocessing/pp.py
+++ src/preprocessing/pp.py
@@ -1,6 +1,7 @@
 # Base class for preprocessing
 import os
 import pandas as pd
+import gc
 
 
 class PP:
@@ -10,46 +11,42 @@ class PP:
     def preprocess(self, data):
         raise NotImplementedError
 
-    def run(self, data, curr, config):
-        # Check if the prev path exists
-        # path = 'data/processed/' + '_'.join(curr) + '.parquet'
-        # if os.path.exists(path):
-        #     print(f'Preprocessed data already exists, reading from {path}')
-        #     # Read the data from the path with polars
-        #     if self.use_pandas:
-        #         processed = pd.read_parquet(path)
-        #     else:
-        #         processed = pl.read_parquet(path)
-        #     print(f'Data read from {path}')
-        # else:
-        #     # Recalculate the current path to save the data
-        #     print('Preprocessed data does not exist, applying preprocessing')
-        #     processed = self.preprocess(data)
-        #     processed.to_parquet(path, compression='zstd')
+    def run(self, data, config):
         processed = data
-        for i in range(len(curr), -1, -1):
-            path = 'data/processed/' + '_'.join(curr[:i]) + '.parquet'
+        steps, step_names = config.get_pp_steps()
+        for i in range(len(step_names), -1, -1):
+            path = config.get_pp_out() + '/' + '_'.join(step_names[:i]) + '.parquet'
             # check if the final result of the preprocessing exists
             if os.path.exists(path):
                 print('Found existing file at:', path)
+                print('Reading from:', path)
                 processed = pd.read_parquet(path)
+                print('Data read from:', path)
                 break
             else:
-                print('File not found at:', path)
+                if i == 0:
+                    print('No files found, reading from:', config.get_pp_in())
+                else:
+                    print('File not found at:', path)
                 # find the latest version of the preprocessing
                 # inside this loop
                 continue
+        # if no files were found, i=0, read the unprocessed data here
+        if i == 0:
+            print('No files found, reading from:', config.get_pp_in())
+            processed = pd.read_parquet(config.get_pp_in() + '/train_series.parquet')
+            print('Data read from:', config.get_pp_in())
         # now using i run the preprocessing steps that were not applied
-        for j, step in enumerate(curr[i:]):
-            path = 'data/processed/' + '_'.join(curr[:i+j+1]) + '.parquet'
+        for j, step in enumerate(step_names[i:]):
+            path = config.get_pp_out() + '/' + '_'.join(step_names[:i+j+1]) + '.parquet'
             # step is the string name of the step to apply
-            # use configloader to read the class
-            steps, _ = config.get_pp_steps()
             step = steps[i+j]
+            print('Applying preprocessing step:', step_names[i+j])
             processed = step.preprocess(processed)
+            gc.collect()
             # save the result
             print('Preprocessing was applied')
             print('Saving to:', path)
             processed.to_parquet(path)
-
+            print('Saved to: ', path)
         return processed
--- test/preprocessing_tests/integration_test_pp.py
+++ test/preprocessing_tests/integration_test_pp.py
@@ -1,30 +1,30 @@
 '''THIS IS NOT MEANT TO BE A UNITTEST'''
 from src.configs.load_config import ConfigLoader
 import time
-import pandas as pd
+from src.preprocessing.pp import PP
 
 
 if __name__ == "__main__":
 
     config = ConfigLoader("test/test_config.json")
-    start_time = time.time()
+    # start_time = time.time()
     # use polars to read parquet because that is significantly faster
-    df = pd.read_parquet(config.get_pp_in() + "/train_series.parquet")
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Elapsed time for reading 32float parquet: {elapsed_time:.6f} seconds")
-    print('memory usage before:')
-    print(df.memory_usage(deep=True).sum()/(1024*1024))
+    # df = pd.read_parquet(config.get_pp_in() + "/train_series.parquet")
+    # end_time = time.time()
+    # elapsed_time = end_time - start_time
+    # print(f"Elapsed time for reading 32float parquet: {elapsed_time:.6f} seconds")
+    # print('memory usage before:')
+    # print(df.memory_usage(deep=True).sum()/(1024*1024))
     # Print the elapsed time
 
     # Initialize preprocessing steps
+    preprocess = PP()
     pp_steps, pp_s = config.get_pp_steps()
-    processed = df
+    processed = None
     # Get the preprocessing steps as a list of str to make the paths
     start_time = time.time()
-    for i, step in enumerate(pp_steps):
-        # Passes the current list because it's needed to write to if the path doesn't exist
-        processed = step.run(processed, pp_s, config)
+    # Passes the current list because it's needed to write to if the path doesn't exist
+    processed = preprocess.run(processed, config)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time for preprocessing: {elapsed_time:.6f} seconds")

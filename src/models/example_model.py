0fd941a5d17c4006b8de1d613e791269a88ca045
hjdeheer <hugodeheer1234@gmail.com>
2023-10-03 16:16:31 +0200

    Add start to unet 1d cnn model

--- src/models/example_model.py
+++ src/models/example_model.py
@@ -1,6 +1,6 @@
 import torch
-import torch.nn as nn
 
+from architectures.simple_model import SimpleModel
 from ..logger.logger import logger
 from ..loss.loss import Loss
 from ..models.model import Model, ModelException
@@ -204,21 +204,3 @@ class ExampleModel(Model):
         self.config = checkpoint['config']
 
         logger.info("Model loaded from: " + path)
-
-
-class SimpleModel(nn.Module):
-    """
-    Pytorch implementation of a really simple baseline model.
-    """
-
-    def __init__(self, input_dim, hidden_dim, output_dim, config):
-        super(SimpleModel, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
-        return x

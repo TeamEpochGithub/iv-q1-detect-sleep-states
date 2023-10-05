faa9f4b904a9d07500badd67ddc9f3fcd2920862
Jeffrey Lim <Jeffrey-Lim@outlook.com>
2023-10-05 11:28:30 +0200

    Add paths to config.json, refactor stuff to make it work, and add comments and type hints

--- src/get_processed_data.py
+++ src/get_processed_data.py
@@ -1,16 +1,19 @@
+from src.configs.load_config import ConfigLoader
 from src.logger.logger import logger
 import os
 import pandas as pd
 import gc
 
 
-def get_processed_data(config, series_path, save_output=True):
-    pp_steps, pp_step_names = config.get_pp_steps()
+def get_processed_data(config: ConfigLoader, training=True, save_output=True) -> pd.DataFrame:
+    pp_steps, pp_step_names = config.get_pp_steps(training=training)
     fe_steps, fe_step_names = config.get_features()
     fe_steps = [fe_steps[key] for key in fe_steps]
     step_names = pp_step_names + fe_step_names
     steps = pp_steps + fe_steps
 
+    i: int = 0
+    processed: pd.DataFrame = pd.DataFrame()
     for i in range(len(step_names), -1, -1):
         path = config.get_pp_out() + '/' + '_'.join(step_names[:i]) + '.parquet'
         # check if the final result of the preprocessing exists
@@ -23,16 +26,17 @@ def get_processed_data(config, series_path, save_output=True):
             logger.debug(f'File not found at: {path}')
 
     if i == 0:
+        series_path = config.get_train_series_path() if training else config.get_test_series_path()
         logger.info(f'No files found, reading from: {series_path}')
         processed = pd.read_parquet(series_path)
         logger.info(f'Data read from: {series_path}')
 
     # now using i run the preprocessing steps that were not applied
     for j, step in enumerate(step_names[i:]):
-        path = config.get_pp_out() + '/' + '_'.join(step_names[:i+j+1]) + '.parquet'
+        path = config.get_pp_out() + '/' + '_'.join(step_names[:i + j + 1]) + '.parquet'
         # step is the string name of the step to apply
-        step = steps[i+j]
-        logger.info(f'--- Applying step: {step_names[i+j]}')
+        step = steps[i + j]
+        logger.info(f'--- Applying step: {step_names[i + j]}')
         processed = step.run(processed)
         gc.collect()
         # save the result
--- src/preprocessing/add_state_labels.py
+++ src/preprocessing/add_state_labels.py
@@ -1,6 +1,5 @@
-'''This class is used to add the state labels like pseudo-NaN, asleep and dont predict to the data'''
 from ..logger.logger import logger
-from src.preprocessing.pp import PP
+from ..preprocessing.pp import PP, PPException
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -9,31 +8,64 @@ import os
 
 
 class AddStateLabels(PP):
+    """Adds state labels to each row of the data
 
-    def preprocess(self, data):
+    The state labels are added to the "awake" column based on the events csv file.
+    The values are 0 for asleep, 1 for awake, and 2 for unlabeled.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Initialize the AddStateLabels class.
+        :param events_path: the path to the events csv file
+        :raises PPException: If no path to the events csv file is given
+        """
+        super().__init__()
+
+        if 'events_path' not in kwargs:
+            logger.critical("No path to labels given. Make sure to add the events_path argument.")
+            raise PPException("No path to labels given. Make sure to add the events_path argument.")
+
+        self.events_path: str = kwargs['events_path']
+        self.events: pd.DataFrame = pd.DataFrame()
+
+    def run(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Run the preprocessing step.
+        :param data: the data to preprocess
+        :return: the preprocessed data
+        :raises FileNotFoundError: If the events csv file is not found
+        """
+        self.events: pd.DataFrame = pd.read_csv(self.events_path)
+        return self.preprocess(data)
+
+    def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Preprocess the data by adding state labels to each row of the data.
+        :param data: the data without state labels
+        :return: the data with state labels added to the "awake" column
+        """
         # Initialize the columns
         data['NaN'] = 0
         data['awake'] = 0
-        # Read the events dataframe
-        events = pd.read_csv('data/raw/train_events.csv')
 
         # apply encoding to events
         if os.path.exists('series_id_encoding.json'):
             f = open('series_id_encoding.json')
             id_encoding = json.load(f)
-        events['series_id'] = events['series_id'].map(id_encoding)
+        self.events['series_id'] = self.events['series_id'].map(id_encoding)
 
-        events_copy = events.copy()
+        events_copy = self.events.copy()
         events_copy.dropna(inplace=True)
         # This part does some needed pp for getting the NaN series
-        series_has_nan = events.groupby(
+        series_has_nan = self.events.groupby(
             'series_id')['step'].apply(lambda x: x.isnull().any())
         series_has_nan.value_counts()
-        df_has_NaN = series_has_nan.to_frame()
-        df_has_NaN.reset_index(inplace=True)
+        df_has_nan = series_has_nan.to_frame()
+        df_has_nan.reset_index(inplace=True)
 
         # Finds series ids without NaN
-        not_nan = df_has_NaN.loc[df_has_NaN.step == 0]["series_id"].to_list()
+        not_nan = df_has_nan.loc[df_has_nan.step == 0]["series_id"].to_list()
         # The series that do not have Nans but have missing labels at the end
         weird_series = ["0cfc06c129cc", "31011ade7c0a",
                         "55a47ff9dc8a", "a596ad0b82aa", "a9a2f7fac455"]
@@ -59,7 +91,7 @@ class AddStateLabels(PP):
         # and add 2s for the awake labels
         for i, id in tqdm(enumerate(weird_series_encoded)):
             # get the events with the current series id
-            current_events = events[events["series_id"] == id]
+            current_events = self.events[self.events["series_id"] == id]
             # get the last item of the current events
             last_event = current_events.tail(1)
             # set awake of current series to 2 for all rows after last_event
@@ -68,7 +100,7 @@ class AddStateLabels(PP):
 
         logger.debug("------ Finished handling weird series")
         # magic code i copied from EDA-Hugo to do the NaN stuff
-        df_filled = events.copy()
+        df_filled = self.events.copy()
         onset_mask = df_filled['event'] == 'onset'
         wakeup_mask = df_filled['event'] == 'wakeup'
         df_filled.loc[onset_mask, 'step'] = df_filled.groupby('series_id')[
@@ -84,8 +116,9 @@ class AddStateLabels(PP):
             current_series = self.get_nan_train_series(
                 current_series, nan_events, id)
             # For some reason the array is longer than the series so only take a slice same size as the series
-            data.loc[data['series_id'] == id, 'NaN'] = current_series['NaN'].to_numpy()[:data.loc[data['series_id'] == id].shape[0]]
-            current_series = self.get_train_series(current_series, events, id)
+            data.loc[data['series_id'] == id, 'NaN'] = current_series['NaN'].to_numpy()[
+                                                       :data.loc[data['series_id'] == id].shape[0]]
+            current_series = self.get_train_series(current_series, self.events, id)
             # after getting the awake column apply shift and fill to
             # set the event occurance properly
             current_series['awake'] = current_series['awake'].shift(-1).ffill()
@@ -111,7 +144,7 @@ class AddStateLabels(PP):
         current_events["pseudo-NaN"] = current_events["event"].replace(
             {"onset": 1, "wakeup": 0})
         train = pd.merge(current_series, current_events[[
-                         'step', 'pseudo-NaN']], on='step', how='left')
+            'step', 'pseudo-NaN']], on='step', how='left')
 
         # Set before filling so setup pseudo-NaN / sleeps
         train["pseudo-NaN"] = train["pseudo-NaN"].bfill(axis='rows')
@@ -140,7 +173,7 @@ class AddStateLabels(PP):
             {"onset": 1, "wakeup": 0})
 
         train = pd.merge(current_series, current_events[[
-                         'step', 'awake']], on='step', how='left')
+            'step', 'awake']], on='step', how='left')
         if 'awake_y' in train.columns:
             train.rename(columns={'awake_y': 'awake'}, inplace=True)
         if 'awake_x' in train.columns:

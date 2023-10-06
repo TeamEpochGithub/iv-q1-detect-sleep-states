165d7d2ae2eb508b26a737fbc1aefd7206813929
Emiel Witting <emielwitting@teamepoch.net>
2023-10-06 13:45:01 +0200

    remove __main__ call

--- src/loss/regression_loss.py
+++ src/loss/regression_loss.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch
 
 
 class RegressionLoss(nn.Module):
@@ -23,23 +22,3 @@ class RegressionLoss(nn.Module):
         loss = loss * mask
 
         return loss.mean()
-
-
-# Main
-if __name__ == "__main__":
-    test_pred = [[123, 444, 0.3, 0.2], [4123, 2235, 0.5, 0.2]]
-    test_true = [[100, 400, 0, 0], [-1, -1, 1, 1]]
-
-    test_pred = torch.tensor(test_pred)
-    test_true = torch.tensor(test_true)
-    loss = RegressionLoss()
-
-    # Create mask from true values
-    # If index 2 is 1 then index 0 is 0 else 1
-    # If index 3 is 1 then index 1 is 0 else 1
-    # Index 2 and 3 always have 1
-    mask = torch.ones_like(test_true)
-    mask[:, 0] = 1 - test_true[:, 2]
-    mask[:, 1] = 1 - test_true[:, 3]
-
-    print(loss(test_pred, test_true, mask))

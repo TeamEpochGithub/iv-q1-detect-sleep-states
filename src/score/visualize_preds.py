ba984b0bbf7b94da7d202b489b6e822436c4fcec
tolgakopar <cahittolgakopar@gmail.com>
2023-10-19 12:17:48 +0200

    Fixed issue with visualizations not working
    In add_segmentation_labels the step column was set to an unsigned int but the padding generates negative steps so that lead to an overflow for each series that was padded.

--- src/score/visualize_preds.py
+++ src/score/visualize_preds.py
@@ -43,114 +43,113 @@ def plot_preds_on_series(preds: pd.DataFrame, data: pd.DataFrame, events_path: s
         number_of_series_to_plot = len(ids_to_plot)
     # Loop through the series ids
     for id in series_ids[:number_of_series_to_plot]:
-        if id is not None:
-            current_series = data[data['series_id'] == id]
-            # Take the events for the current series
-            current_events = real_events.loc[real_events['series_id'] == id]
-            # Take the preds for the current series
-            current_preds = preds.loc[preds['series_id'] == id]
-            # Separate the series by awake values to plot with different colors
-            awake_0_df = current_series[current_series['awake'] == 0]
-            awake_1_df = current_series[current_series['awake'] == 1]
-            awake_2_df = current_series[current_series['awake'] == 2]
+        current_series = data[data['series_id'] == id]
+        # Take the events for the current series
+        current_events = real_events.loc[real_events['series_id'] == id]
+        # Take the preds for the current series
+        current_preds = preds.loc[preds['series_id'] == id]
+        # Separate the series by awake values to plot with different colors
+        awake_0_df = current_series[current_series['awake'] == 0]
+        awake_1_df = current_series[current_series['awake'] == 1]
+        awake_2_df = current_series[current_series['awake'] == 2]
 
-            # The steps of the dataframes awake-0_df will have diff > 1 for when a jump occurs
-            # We need to find the indices of the jumps and use the indices to give them a group number
-            # Then using .groupby('group') we can plot the groups separately
-            # This way the jumps will not be connected
+        # The steps of the dataframes awake-0_df will have diff > 1 for when a jump occurs
+        # We need to find the indices of the jumps and use the indices to give them a group number
+        # Then using .groupby('group') we can plot the groups separately
+        # This way the jumps will not be connected
 
-            # Writing this part in place was going to be too hard so i didnt do it
-            # This way works but gives warnings about SettingWithCopyWarning
-            # So we just ignore those warnings
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                # get the diff of the steps
-                awake_0_df['diff'] = awake_0_df['step'].diff()
-                # get the indices of the jumps
-                jump_indices = awake_0_df[awake_0_df['diff'] > 1].index
-                # create a new column with the group number
-                awake_0_df['group'] = 0
-                # loop through the jump indices and increment the group number
-                for i in jump_indices:
-                    awake_0_df.loc[i:, 'group'] += 1
+        # Writing this part in place was going to be too hard so i didnt do it
+        # This way works but gives warnings about SettingWithCopyWarning
+        # So we just ignore those warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            # get the diff of the steps
+            awake_0_df['diff'] = awake_0_df['step'].diff()
+            # get the indices of the jumps
+            jump_indices = awake_0_df[awake_0_df['diff'] > 1].index
+            # create a new column with the group number
+            awake_0_df['group'] = 0
+            # loop through the jump indices and increment the group number
+            for i in jump_indices:
+                awake_0_df.loc[i:, 'group'] += 1
 
-                awake_1_df['diff'] = awake_1_df['step'].diff()
-                jump_indices = awake_1_df[awake_1_df['diff'] > 1].index
-                awake_1_df['group'] = 0
-                for i in jump_indices:
-                    awake_1_df.loc[i:, 'group'] += 1
+            awake_1_df['diff'] = awake_1_df['step'].diff()
+            jump_indices = awake_1_df[awake_1_df['diff'] > 1].index
+            awake_1_df['group'] = 0
+            for i in jump_indices:
+                awake_1_df.loc[i:, 'group'] += 1
 
-                awake_2_df['diff'] = awake_2_df['step'].diff()
-                jump_indices = awake_2_df[awake_2_df['diff'] > 1].index
-                awake_2_df['group'] = 0
-                for i in jump_indices:
-                    awake_2_df.loc[i:, 'group'] += 1
+            awake_2_df['diff'] = awake_2_df['step'].diff()
+            jump_indices = awake_2_df[awake_2_df['diff'] > 1].index
+            awake_2_df['group'] = 0
+            for i in jump_indices:
+                awake_2_df.loc[i:, 'group'] += 1
 
-            # Create the figure
-            fig = go.Figure()
-            # If features_to_plot is None, plot all the features
-            if features_to_plot is None:
-                features_to_plot = data.columns.values
-            for feature_to_plot in features_to_plot:
-                # Some features are not meant to be plotted like step, series_id, awake, timestamp
-                if feature_to_plot != 'step' and feature_to_plot != 'series_id' and feature_to_plot != 'awake' and feature_to_plot != 'timestamp':
-                    # This part plots the features for awake=0, awake=1, awake=2 in a way that the jumps are not connected
-                    # and we get only 1 legend item for the entire group by showing legend only on the first trace
-                    awake_0_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
-                                                                                         y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=0',
-                                                                                         line=dict(color='blue'), legendgroup=feature_to_plot+'Awake=0',
-                                                                                         showlegend=True if x.name == 0 else False)))
-                    awake_1_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
-                                                                                         y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=1',
-                                                                                         line=dict(color='red'), legendgroup=feature_to_plot+'Awake=1',
-                                                                                         showlegend=True if x.name == 0 else False)))
-                    awake_2_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
-                                                                                         y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=2',
-                                                                                         line=dict(color='green'), legendgroup=feature_to_plot+'Awake=2',
-                                                                                         showlegend=True if x.name == 0 else False)))
-                    fig.update_xaxes(title='Timestamp')
-                    fig.update_yaxes(title='Feature values')
-                    fig.update_layout(
-                        title=f'Anglez for Series ID: {id_decoding[id]}-{id}')
-                    fig.update_xaxes(tickvals=current_series['step'][::len(
-                        current_series) // 10], tickangle=45)
+        # Create the figure
+        fig = go.Figure()
+        # If features_to_plot is None, plot all the features
+        if features_to_plot is None:
+            features_to_plot = data.columns.values
+        for feature_to_plot in features_to_plot:
+            # Some features are not meant to be plotted like step, series_id, awake, timestamp
+            if feature_to_plot == 'anglez' or feature_to_plot == 'enmo' or 'f_' in feature_to_plot:
+                # This part plots the features for awake=0, awake=1, awake=2 in a way that the jumps are not connected
+                # and we get only 1 legend item for the entire group by showing legend only on the first trace
+                awake_0_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
+                                                                                        y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=0',
+                                                                                        line=dict(color='blue'), legendgroup=feature_to_plot+'Awake=0',
+                                                                                        showlegend=True if x.name == 0 else False)))
+                awake_1_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
+                                                                                        y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=1',
+                                                                                        line=dict(color='red'), legendgroup=feature_to_plot+'Awake=1',
+                                                                                        showlegend=True if x.name == 0 else False)))
+                awake_2_df.groupby('group').apply(lambda x: fig.add_trace(go.Scatter(x=x['step'],
+                                                                                        y=x[feature_to_plot], mode='lines', name=feature_to_plot+'Awake=2',
+                                                                                        line=dict(color='green'), legendgroup=feature_to_plot+'Awake=2',
+                                                                                        showlegend=True if x.name == 0 else False)))
+                fig.update_xaxes(title='Timestamp')#, range=[0, current_series.shape[0]])
+                fig.update_yaxes(title='Feature values')
+                fig.update_layout(
+                    title=f'Anglez for Series ID: {id_decoding[id]}-{id}')
+                fig.update_xaxes(tickvals=current_series['step'][::len(
+                    current_series) // 10], tickangle=45)
 
-            # Before showing the figure make vertical lines for the current_events and current_preds
-            # the vertical lines have annotations that show the real and predicted values and the timestamps
-            # and the annotations are colored according to the event type
-            for current_onset in current_events[current_events['event'] == 'onset']['step'].dropna():
-                fig.add_vline(x=current_onset, line_dash="dash", line_color="black", line_width=2,
-                              annotation_text=f'<span style="color:black">real_onset:<br> {current_onset}</span>',
-                              annotation_position="top", name=f'Vertical Line at x={current_onset}', annotation_textangle=315,
-                              )
-            for current_wakeup in current_events[current_events['event'] == 'wakeup']['step'].dropna():
-                fig.add_vline(x=current_wakeup, line_dash="dash", line_color="green", name="wakeup", line_width=2,
-                              annotation_text=f'<span style="color:green">real_wakeup:<br> {current_wakeup}</span>',
-                              annotation_position="top", annotation_textangle=315)
+        # Before showing the figure make vertical lines for the current_events and current_preds
+        # the vertical lines have annotations that show the real and predicted values and the timestamps
+        # and the annotations are colored according to the event type
+        for current_onset in current_events[current_events['event'] == 'onset']['step'].dropna():
+            fig.add_vline(x=current_onset, line_dash="dash", line_color="black", line_width=2,
+                            annotation_text=f'<span style="color:black">real_onset:<br> {current_onset}</span>',
+                            annotation_position="top", name=f'Vertical Line at x={current_onset}', annotation_textangle=315,
+                            )
+        for current_wakeup in current_events[current_events['event'] == 'wakeup']['step'].dropna():
+            fig.add_vline(x=current_wakeup, line_dash="dash", line_color="green", name="wakeup", line_width=2,
+                            annotation_text=f'<span style="color:green">real_wakeup:<br> {current_wakeup}</span>',
+                            annotation_position="top", annotation_textangle=315)
 
-            for current_pred_onset in current_preds[current_preds['event'] == 'onset']['step'].dropna():
-                fig.add_vline(x=current_pred_onset, line_dash="dash", line_color="red", line_width=2,
-                              annotation_text=f'<span style="color:red">pred_onset:<br> {current_pred_onset}</span>',
-                              annotation_position="top", name=f'Vertical Line at x={current_pred_onset}', annotation_textangle=315,
-                              )
-            for current_pred_wakeup in current_preds[current_preds['event'] == 'wakeup']['step'].dropna():
-                fig.add_vline(x=current_pred_wakeup, line_dash="dash", line_color="orange", name="wakeup", line_width=2,
-                              annotation_text=f'<span style="color:orange"> pred_wakeup:<br> {current_pred_wakeup}</span>',
-                              annotation_position="top", annotation_textangle=315,
-                              )
-            # The normal margin doesnt work for the annotations
-            fig.update_layout(margin=dict(t=160))
-            if show_plot:
-                fig.show()
-            else:
-                # This downsamples the plotly figure
-                # It is not necessary but it makes the saving 8x faster
-                fig = FigureResampler(fig)
-                # If the hash config dir doesnt exist make it
-                if not os.path.exists(folder_path):
-                    os.makedirs(folder_path)
-                fig.write_image(folder_path + '/' + 'series_id--' +
-                                f'{id_decoding[id]}-({id}).jpeg', width=2000, height=600)
+        for current_pred_onset in current_preds[current_preds['event'] == 'onset']['step'].dropna():
+            fig.add_vline(x=current_pred_onset, line_dash="dash", line_color="red", line_width=2,
+                            annotation_text=f'<span style="color:red">pred_onset:<br> {current_pred_onset}</span>',
+                            annotation_position="top", name=f'Vertical Line at x={current_pred_onset}', annotation_textangle=315,
+                            )
+        for current_pred_wakeup in current_preds[current_preds['event'] == 'wakeup']['step'].dropna():
+            fig.add_vline(x=current_pred_wakeup, line_dash="dash", line_color="orange", name="wakeup", line_width=2,
+                            annotation_text=f'<span style="color:orange"> pred_wakeup:<br> {current_pred_wakeup}</span>',
+                            annotation_position="top", annotation_textangle=315,
+                            )
+        # The normal margin doesnt work for the annotations
+        fig.update_layout(margin=dict(t=160))
+        if show_plot:
+            fig.show()
+        else:
+            # This downsamples the plotly figure
+            # It is not necessary but it makes the saving 8x faster
+            fig = FigureResampler(fig)
+            # If the hash config dir doesnt exist make it
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            fig.write_image(folder_path + '/' + 'series_id--' +
+                            f'{id_decoding[id]}-({id}).jpeg', width=2000, height=600)
 
 
 if __name__ == "__main__":
@@ -162,4 +161,4 @@ if __name__ == "__main__":
     featured_data = get_processed_data(config, series_path, save_output=True)
     # Plot the predictions on the series data for the chosen series_ids
     plot_preds_on_series(preds, featured_data,
-                         ids_to_plot=[15], show_plot=True)
+                         number_of_series_to_plot=5, show_plot=True)

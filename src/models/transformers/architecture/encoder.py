5a5c8f761705658a45ebc70c289562a2137b1d58
JasperVS <jmvanselm@gmail.com>
2023-10-24 15:35:54 +0200

    Fix until training

--- src/models/transformers/architecture/encoder.py
+++ src/models/transformers/architecture/encoder.py
@@ -7,13 +7,15 @@ from torch.nn.modules import BatchNorm1d
 import copy
 import torch
 
+
 class Encoder(nn.Module):
     def __init__(self, tokenizer, pe, emb_dim, forward_dim, n_layers, heads):
         super().__init__()
         self.n_layers = n_layers
         self.tokenizer = tokenizer
         self.pe = pe
-        self.layers = get_clones(EncoderLayer(emb_dim, heads, forward_dim), n_layers)
+        self.layers = get_clones(EncoderLayer(
+            emb_dim, heads, forward_dim), n_layers)
         encoders = []
         for _ in range(0, n_layers):
             encoders.append(
@@ -21,11 +23,13 @@ class Encoder(nn.Module):
             )
         self.encoder_stack = nn.Sequential(*encoders)
         self.norm = BatchNorm1d(emb_dim)
+
     def forward(self, src):
         x = self.tokenizer(src)
         x = self.pe(x)
         x = self.encoder_stack(x)
-        return self.norm(x)
-    
+        return self.norm(x.permute(0, 2, 1)).permute(0, 2, 1)
+
+
 def get_clones(module, n_layers):
     return nn.ModuleList([copy.deepcopy(module) for i in range(n_layers)])

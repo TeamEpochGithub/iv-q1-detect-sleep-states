a4eda6d6c5a1b8c35de97d6233da351845e57255
Emiel Witting <emielwitting@teamepoch.net>
2023-10-25 11:15:16 +0200

    more fixes

--- src/preprocessing/similarity_nan.py
+++ src/preprocessing/similarity_nan.py
@@ -7,7 +7,7 @@ import pandas as pd
 class SimilarityNan(PP):
 
     def __init__(self, as_feature=False, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(**kwargs | {"kind": "similarity_nan"})
         self.as_feature = as_feature
 
     def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:

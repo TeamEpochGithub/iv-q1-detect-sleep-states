3fd73dfe0c4056ea4a6e20b45e08fd136996bb32
Jeffrey Lim <Jeffrey-Lim@outlook.com>
2023-11-03 17:05:15 +0100

    Fix a few bugs (still more in here)

--- src/preprocessing/remove_unlabeled.py
+++ src/preprocessing/remove_unlabeled.py
@@ -12,21 +12,23 @@ class RemoveUnlabeled(PP):
     If the "window" column in present, only drop the windows where all the "awake" values are 3 or 2.
     """
 
-    def __init__(self, remove_only_full_windows: bool, keep_nan: bool, **kwargs: dict) -> None:
+    def __init__(self, remove_only_fully_unlabeled_windows: bool, remove_nan: bool, remove_series: bool, **kwargs: dict) -> None:
         """Initialize the RemoveUnlabeled class
 
-        :param remove_only_full_windows: Only applies when windowing is used.
+        :param remove_only_fully_unlabeled_windows: Only applies when windowing is used.
             If True, remove all windows that contain unlabeled data.
             If False, only remove the fully unlabeled windows.
-        :param keep_nan: If false, remove the NaN data too. If True, keep the NaN data.
+        :param remove_nan: If false, remove the NaN data too. If True, keep the NaN data.
+        :param remove_series: If True, remove all series that contain unlabeled data. Ignores windowing.
         """
         super().__init__(**kwargs | {"kind": "remove_unlabeled"})
-        self.remove_only_full_windows = remove_only_full_windows
-        self.keep_nan = keep_nan
+        self.remove_only_fully_unlabeled_windows = remove_only_fully_unlabeled_windows
+        self.remove_nan = remove_nan
+        self.remove_series = remove_series
 
     def __repr__(self) -> str:
         """Return a string representation of a RemoveUnlabeled object"""
-        return f"{self.__class__.__name__}(remove_only_full_windows={self.remove_only_full_windows}, keep_nan={self.keep_nan})"
+        return f"{self.__class__.__name__}(remove_only_fully_unlabeled_windows={self.remove_only_fully_unlabeled_windows}, keep_nan={self.remove_nan})"
 
     def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
         """Removes all the data points where there is no labeled data
@@ -40,23 +42,35 @@ class RemoveUnlabeled(PP):
             logger.critical("No awake column. Did you run AddStateLabels before?")
             raise PPException("No awake column. Did you run AddStateLabels before?")
 
-        if "window" in data.columns:
-            logger.info("------ Removing NaN data with windowing")
-            if self.remove_only_full_windows:
-                data = data.groupby(["window"]).filter(lambda x: not (x['awake'] == 3).all()).reset_index(drop=True)
-            else:
-                data = data.groupby(["window"]).filter(lambda x: (x['awake'] != 3).any()).reset_index(drop=True)
+        logger.info(f"------ Data shape before: {data.shape}")
 
-            if not self.keep_nan:
-                if self.remove_only_full_windows:
-                    data = data.groupby(["window"]).filter(lambda x: not (x['awake'] == 2).all()).reset_index(drop=True)
-                else:
-                    data = data.groupby(["window"]).filter(lambda x: (x['awake'] != 2).any()).reset_index(drop=True)
+        if self.remove_series:
+            data = data.groupby(["series_id"]).filter(lambda x: (x['awake'] != 3).all()).reset_index(drop=True)
+            if self.remove_nan:
+                data = data.groupby(["series_id"]).filter(lambda x: (x['awake'] != 2).all()).reset_index(drop=True)
+
+            logger.info(f"------ Data shape after: {data.shape}")
             return data
 
-        logger.info("------ Removing NaN data without windowing")
+        if "window" in data.columns:
+            logger.info("------ Removing unlabeled data with windowing")
+            if self.remove_only_fully_unlabeled_windows:
+                data = data.groupby(["window"]).filter(lambda x: (x['awake'] != 3).any()).reset_index(drop=True)
+            else:
+                data = data.groupby(["window"]).filter(lambda x: not (x['awake'] == 3).any()).reset_index(drop=True)
+
+            if self.remove_nan:
+                if self.remove_only_fully_unlabeled_windows:
+                    data = data.groupby(["window"]).filter(lambda x: (x['awake'] != 2).any()).reset_index(drop=True)
+                else:
+                    data = data.groupby(["window"]).filter(lambda x: not (x['awake'] == 2).any()).reset_index(drop=True)
+            logger.info(f"------ Data shape after: {data.shape}")
+            return data
+
+        logger.info("------ Removing unlabeled data without windowing")
         data = data[(data["awake"] != 3)].reset_index(drop=True)
-        if not self.keep_nan:
+        if self.remove_nan:
             data = data[(data["awake"] != 2)].reset_index(drop=True)
 
+        logger.info(f"------ Data shape after: {data.shape}")
         return data
--- test/preprocessing_tests/test_remove_unlabeled.py
+++ test/preprocessing_tests/test_remove_unlabeled.py
@@ -13,7 +13,7 @@ class TestRemoveUnlabeled(TestCase):
     Tests the RemoveUnlabeled class.
     """
 
-    remove_unlabeled = RemoveUnlabeled(remove_only_full_windows=True, keep_nan=False)
+    remove_unlabeled = RemoveUnlabeled(remove_only_fully_unlabeled_windows=True, remove_nan=False, remove_series=False)
 
     def test_no_state(self) -> None:
         df: pd.DataFrame = pd.DataFrame({
@@ -43,7 +43,7 @@ class TestRemoveUnlabeled(TestCase):
         self.assertEqual(11, df_test.shape[0])
 
     def test_no_window_2(self) -> None:
-        remove_unlabeled = RemoveUnlabeled(remove_only_full_windows=False, keep_nan=True)
+        remove_unlabeled = RemoveUnlabeled(remove_only_fully_unlabeled_windows=False, remove_nan=True, remove_series=False)
         df: pd.DataFrame = pd.DataFrame({
             "series_id": np.repeat("test", 20),
             "step": range(20),
@@ -56,6 +56,21 @@ class TestRemoveUnlabeled(TestCase):
 
         df_test: pd.DataFrame = remove_unlabeled.preprocess(df)
         self.assertEqual(16, df_test.shape[0])
+
+    def test_no_window_3(self) -> None:
+        remove_unlabeled = RemoveUnlabeled(remove_only_fully_unlabeled_windows=False, remove_nan=True, remove_series=True)
+        df: pd.DataFrame = pd.DataFrame({
+            "series_id": np.concatenate((np.repeat("test", 10), np.repeat("test2", 10))),
+            "step": range(20),
+            "timestamp": pd.date_range(datetime.today(), periods=20, freq="5S"),
+            "anglez": np.random.uniform(2, 5, 20),
+            "enmo": np.random.uniform(0, 1, 20),
+            "awake": np.concatenate(
+                (np.repeat(1, 3), np.repeat(0, 2), np.repeat(2, 5), np.repeat(0, 6), np.repeat(3, 4))),
+        })
+
+        df_test: pd.DataFrame = remove_unlabeled.preprocess(df)
+        self.assertEqual(0, df_test.shape[0])
 
     def test_window(self) -> None:
         df: pd.DataFrame = pd.DataFrame({
@@ -72,6 +87,23 @@ class TestRemoveUnlabeled(TestCase):
 
         df_test: pd.DataFrame = self.remove_unlabeled.preprocess(df)
         self.assertEqual(13, df_test.shape[0])
+
+    def test_window_2(self) -> None:
+        remove_unlabeled = RemoveUnlabeled(remove_only_fully_unlabeled_windows=False, remove_nan=True, remove_series=False)
+        df: pd.DataFrame = pd.DataFrame({
+            "series_id": np.repeat("test", 20),
+            "step": range(20),
+            "timestamp": pd.date_range(datetime.today(), periods=20, freq="5S"),
+            "anglez": np.random.uniform(2, 5, 20),
+            "enmo": np.random.uniform(0, 1, 20),
+            "awake": np.concatenate(
+                (np.repeat(1, 2), np.repeat(0, 5), np.repeat(2, 8), np.repeat(0, 2), np.repeat(3, 3))),
+            "window": np.concatenate(
+                (np.repeat(1, 3), np.repeat(2, 5), np.repeat(3, 5), np.repeat(4, 5), np.repeat(5, 2))),
+        })
+
+        df_test: pd.DataFrame = remove_unlabeled.preprocess(df)
+        self.assertEqual(3, df_test.shape[0])
 
     def test_window_unchanged(self) -> None:
         df: pd.DataFrame = pd.DataFrame({

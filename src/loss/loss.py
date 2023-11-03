Fix bugs in event seg for masking

src/loss/loss.py
@@ -74,7 +74,7 @@ class ShrinkageLoss(nn.Module):
         super(ShrinkageLoss, self).__init__()
         self.reduction = reduction
 
-    def forward(self, inputs, targets, alpha: int = 2, gamma: int = 2, c: int = 0.2, smooth=1):
+    def forward(self, inputs, targets, alpha: int = 2, c: int = 0.2):
 
         l1_loss = torch.abs(inputs - targets)
         shrinkage_loss = ((l1_loss) ** 2) * torch.exp(targets) / \

src/models/event_seg_unet_1d_cnn.py
@@ -152,10 +152,16 @@ class EventSegmentationUnet1DCNN(Model):
         X_test = torch.from_numpy(X_test).permute(0, 2, 1)
 
         # Get only the 2 event state features
-        y_train = y_train[:, :, np.array(
-            [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
-        y_test = y_test[:, :, np.array(
-            [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+        if mask_unlabeled:
+            y_train = y_train[:, :, np.array(
+                [data_info.y_columns["awake"], data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+            y_test = y_test[:, :, np.array(
+                [data_info.y_columns["awake"], data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+        else:
+            y_train = y_train[:, :, np.array(
+                [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+            y_test = y_test[:, :, np.array(
+                [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
         y_train = torch.from_numpy(y_train).permute(0, 2, 1)
         y_test = torch.from_numpy(y_test).permute(0, 2, 1)
 
@@ -184,11 +190,11 @@ class EventSegmentationUnet1DCNN(Model):
         trainer = EventTrainer(
             epochs, criterion, mask_unlabeled, early_stopping)
         avg_losses, avg_val_losses, total_epochs = trainer.fit(
-            trainloader=train_dataloader, testloader=test_dataloader, model=self.model, optimizer=optimizer, device=self.device)
+            trainloader=train_dataloader, testloader=test_dataloader, model=self.model, optimizer=optimizer, name=self.name)
 
         # Log full train and test plot
         if wandb.run is not None:
-            self.log_train_test(avg_losses, avg_val_losses, total_epochs)
+            self.log_train_test(avg_losses, avg_val_losses, len(avg_losses))
         logger.info("--- Training of model complete!")
 
         # Set total_epochs in config if broken by the early stopping
@@ -220,9 +226,12 @@ class EventSegmentationUnet1DCNN(Model):
 
         X_train = torch.from_numpy(X_train).permute(0, 2, 1)
 
-        # Get only the event state features
-        y_train = y_train[:, :, np.array(
-            [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+        if mask_unlabeled:
+            y_train = y_train[:, :, np.array(
+                [data_info.y_columns["awake"], data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
+        else:
+            y_train = y_train[:, :, np.array(
+                [data_info.y_columns["state-onset"], data_info.y_columns["state-wakeup"]])]
         y_train = torch.from_numpy(y_train).permute(0, 2, 1)
 
         # Create a dataset from X and y
@@ -242,7 +251,7 @@ class EventSegmentationUnet1DCNN(Model):
         logger.info("--- Training model full " + self.name)
         trainer = EventTrainer(epochs, criterion, mask_unlabeled, -1)
         trainer.fit(trainloader=train_dataloader, testloader=None,
-                    model=self.model, optimizer=optimizer, device=self.device)
+                    model=self.model, optimizer=optimizer, name=self.name)
 
         logger.info("--- Full train complete!")
 

src/models/trainers/event_trainer.py
@@ -11,9 +11,16 @@ from ... import data_info
 def masked_loss(criterion, outputs, y):
     assert y.shape[1] > 1, "Masked loss only works with shape (batch_size, 2 | 3 depending on both awake and onset, seq_len)"
 
-    labels = y[:, 1, :]
+    labels = y[:, 1:, :]
 
-    unlabeled_mask = y[:, 0, :]
+    # Get the mask from y (shape (batch_size, 2, seq_len)) if y.shape[1] == 3 else (batch_size, 1, seq_len)
+    if y.shape[1] == 3:
+        # Mask is should be two times y[:,0,:] so shape is (batch_size, 2, seq_len)
+        unlabeled_mask = torch.stack([y[:, 0, :], y[:, 0, :]], dim=1)
+    else:
+        # Mask is should be one time y[:,0,:] so shape is (batch_size, 1, seq_len)
+        unlabeled_mask = y[:, 0, :]
+
     # If the mask is 1, keep data, else set to 0
     # Do this if value is 3 (unlabeled), else set to 1
     unlabeled_mask = unlabeled_mask == 3
@@ -112,8 +119,8 @@ class EventTrainer:
                 avg_val_losses.append(val_loss.cpu())
 
             if wandb.run is not None and not full_train:
-                wandb.log({f"Train {str(self.criterion)} of {name}": train_loss,
-                           f"Validation {str(self.criterion)} of {name}": val_loss, "epoch": epoch})
+                wandb.log({f"{data_info.substage} - Train {str(self.criterion)} of {name}": train_loss,
+                           f"{data_info.substage} - Validation {str(self.criterion)} of {name}": val_loss, "epoch": epoch})
 
             # Save model if validation loss is lower than previous lowest validation loss
             if not full_train and val_loss < lowest_val_loss:
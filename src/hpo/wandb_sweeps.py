1021eee1d9e336f4e4b41bd93ccb24a2cb967b97
Jeffrey Lim <Jeffrey-Lim@outlook.com>
2023-11-08 16:30:32 +0100

    Add count in the config

--- src/hpo/wandb_sweeps.py
+++ src/hpo/wandb_sweeps.py
@@ -10,19 +10,21 @@ from src.util.hash_config import hash_config
 class WandBSweeps(HPO):
     """Hyperparameter optimization using Weights & Biases Sweeps."""
 
-    def __init__(self, sweep_configuration: dict[str]) -> None:
+    def __init__(self, sweep_configuration: dict[str], count: int | None = None) -> None:
         """Initialize the hyperparameter optimization method.
 
         :param sweep_configuration: the configuration for the Weights & Biases Sweeps
+        :param count: the number of runs to execute, or None to run infinitely
         """
         super().__init__()
         self.sweep_configuration = sweep_configuration
+        self.count = count
 
         model_name: str = list(sweep_configuration["parameters"]["models"]["parameters"].keys())[0]
         self.sweep_configuration["name"] = f"{model_name}/{hash_config(sweep_configuration, length=16)}"
         self.sweep_id = wandb.sweep(sweep=self.sweep_configuration, project="detect-sleep-states")
 
-    def optimize(self, to_optimize: Callable):
+    def optimize(self, to_optimize: Callable) -> None:
         """Optimize the hyperparameters for a single model with Weights & Biases Sweeps.
 
         Gotta Sweep, Sweep, Sweep!
@@ -30,5 +32,5 @@ class WandBSweeps(HPO):
         :param to_optimize: the function that runs the preprocessing, feature engineering, pretrain, training, and cross validation.
         """
         logger.info("Optimizing hyperparameters with Weights & Biases Sweeps")
-        wandb.agent(self.sweep_id, function=to_optimize)
+        wandb.agent(self.sweep_id, function=to_optimize, count=self.count)
         logger.info("Hyperparameter optimization complete")

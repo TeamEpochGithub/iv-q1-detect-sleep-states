52746de8ef8ad666696653e4459f821ac00e922b
tolgakopar <cahittolgakopar@gmail.com>
2023-11-10 09:51:00 +0100

    added normalization to spectrograms

--- src/models/architectures/spectrogram_encoder_decoder.py
+++ src/models/architectures/spectrogram_encoder_decoder.py
@@ -11,7 +11,12 @@ class SpectrogramEncoderDecoder(nn.Module):
 
     def __init__(self, in_channels, out_channels, model_type, config):
         super(SpectrogramEncoderDecoder, self).__init__()
-        self.spectrogram = T.Spectrogram(n_fft=config.get('n_fft', 127), hop_length=config.get('hop_length', 1))
+        self.spectrogram = nn.Sequential(
+            T.Spectrogram(n_fft=config.get('n_fft', 127), hop_length=config.get('hop_length', 1)),
+            T.AmplitudeToDB(top_db=80),
+            SpecNormalize(),
+        )
+        
         self.encoder = Unet(
             encoder_name=config.get('encoder_name', 'resnet34'),
             encoder_weights=config.get('encoder_weights', 'imagenet'),
@@ -33,3 +38,16 @@ class SpectrogramEncoderDecoder(nn.Module):
         x_encoded = self.encoder(x_spec).squeeze(1)
         y = self.decoder(x_encoded)
         return y.permute(0, 2, 1)
+
+
+class SpecNormalize(nn.Module):
+    def __init__(self, eps: float = 1e-8):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, x):
+        # x: (batch, channel, freq, time)
+        min_ = x.min(dim=-1, keepdim=True)[0].min(dim=-2, keepdim=True)[0]
+        max_ = x.max(dim=-1, keepdim=True)[0].max(dim=-2, keepdim=True)[0]
+
+        return (x - min_) / (max_ - min_ + self.eps)

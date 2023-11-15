9672ff9a946705074a97b2142fb405bdf26dd0b7
tolgakopar <cahittolgakopar@gmail.com>
2023-11-15 16:20:24 +0100

    Revert "Delete test cv"
    This reverts commit 1dfef47ba116639049ce74d0c4e1a5e465811f41.

---
+++ test/cv_tests/test_cv.py
@@ -0,0 +1,26 @@
+from collections.abc import Callable
+from unittest import TestCase
+
+from src.cv.cv import _get_scoring, CV
+
+
+class Test(TestCase):
+    def test_get_scoring(self):
+        scorer = _get_scoring(lambda x: x)
+        self.assertIsInstance(scorer, Callable)
+
+        scorer = _get_scoring("score_full")
+        self.assertIsInstance(scorer, Callable)
+
+        scorer = _get_scoring(["score_full", "score_clean"])
+        self.assertIsInstance(scorer, list)
+
+    def test_cv_init(self):
+        cv = CV(splitter="group_k_fold", scoring="score_full", splitter_params={"n_splits": 5})
+        self.assertEqual(cv.splitter.n_splits, 5)
+        self.assertEqual(cv.splitter.__class__.__name__, "GroupKFold")
+
+        cv = CV(splitter="group_shuffle_split", scoring=["score_full", "score_clean"],
+                splitter_params={"n_splits": 5})
+        self.assertEqual(cv.splitter.n_splits, 5)
+        self.assertEqual(cv.splitter.__class__.__name__, "GroupShuffleSplit")

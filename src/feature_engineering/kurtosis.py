462f31ae5502eb23b29107d0562c626c7710e443
tolgakopar <cahittolgakopar@gmail.com>
2023-11-20 13:53:16 +0100

    removed imports from pp and kurtosis refactor

--- src/feature_engineering/kurtosis.py
+++ src/feature_engineering/kurtosis.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-import pandas as pd
+from tqdm import tqdm
 
 from .rolling_window import RollingWindow
 from ..logger.logger import logger
@@ -11,7 +11,7 @@ class Kurtosis(RollingWindow):
     # TODO Add docstrings for the class, feature_engineering and kurtosis functions
     # TODO Add tests
 
-    def feature_engineering(self, data: pd.DataFrame) -> pd.DataFrame:
+    def feature_engineering(self, data: dict) -> dict:
         # Loop through window sizes
         logger.debug("------ All features: " + str(self.features))
         for feature in self.features:
@@ -23,21 +23,22 @@ class Kurtosis(RollingWindow):
         return data
 
     # Create rolling window features for kurtosis
-    def kurtosis(self, data: pd.DataFrame, window_size: int, feature: str) -> pd.DataFrame:
+    def kurtosis(self, data: dict, window_size: int, feature: str) -> dict:
         # Create a rolling window for kurtosis per series_id
-        data["f_kurtosis_" + feature + "_" + str(window_size)] = data.groupby("series_id")[feature].rolling(
-            window_size).kurt().reset_index(0, drop=True)
+        for sid in tqdm(data.keys()):
+            data[sid]["f_kurtosis_" + feature + "_" + str(window_size)] = data[sid][feature].rolling(
+                window_size).kurt().reset_index(0, drop=True)
 
-        # Make sure there are no NaN values turn them into 0
-        data["f_kurtosis_" + feature + "_" + str(window_size)] = data[
-            "f_kurtosis_" + feature + "_" + str(window_size)].fillna(0.0)
+            # Make sure there are no NaN values turn them into 0
+            data[sid]["f_kurtosis_" + feature + "_" + str(window_size)] = data[sid][
+                "f_kurtosis_" + feature + "_" + str(window_size)].fillna(0.0)
 
-        # Clip kurtosis
-        data["f_kurtosis_" + feature + "_" + str(window_size)] = data[
-            "f_kurtosis_" + feature + "_" + str(window_size)].clip(
-            lower=data["f_kurtosis_" + feature + "_" + str(window_size)].mean() - 5 * data[
-                "f_kurtosis_" + feature + "_" + str(window_size)].std(),
-            upper=data["f_kurtosis_" + feature + "_" + str(window_size)].mean() + 5 * data[
-                "f_kurtosis_" + feature + "_" + str(window_size)].std())
+            # Clip kurtosis
+            data[sid]["f_kurtosis_" + feature + "_" + str(window_size)] = data[sid][
+                "f_kurtosis_" + feature + "_" + str(window_size)].clip(
+                lower=data[sid]["f_kurtosis_" + feature + "_" + str(window_size)].mean() - 5 * data[sid][
+                    "f_kurtosis_" + feature + "_" + str(window_size)].std(),
+                upper=data[sid]["f_kurtosis_" + feature + "_" + str(window_size)].mean() + 5 * data[sid][
+                    "f_kurtosis_" + feature + "_" + str(window_size)].std())
 
         return data

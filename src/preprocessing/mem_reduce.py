462f2cf88e51c7058af7017ff4082887179a0f31
tolgakopar <cahittolgakopar@gmail.com>
2023-11-20 11:59:19 +0100

    using a loop makes mem reduce use less memory

--- src/preprocessing/mem_reduce.py
+++ src/preprocessing/mem_reduce.py
@@ -55,7 +55,6 @@ class MemReduce(PP):
         else:
             logger.debug(f"------ Saving series encoding to {self.id_encoding_path}")
 
-            # TODO Don't write the file here to make this method testable
             with open(self.id_encoding_path, 'w') as f:
                 json.dump(encoding, f)
 
@@ -82,6 +81,11 @@ class MemReduce(PP):
         gc.collect()
         # make a dictionary of the series id and the data
         # without the series_id column because it is the key for that series anyway
-        dfs_dict = {key: group.iloc[:, 1:] for key, group in data.groupby('series_id')}
+        dfs_dict = {}
+        # uses less memeory than this
+        # dfs_dict = {key: group.iloc[:, 1:] for key, group in data.groupby('series_id')}
+        for sid in encoding.values():
+            dfs_dict[sid] = data[data['series_id'] == sid].drop(columns=['series_id'])
+        del data
         gc.collect()
         return dfs_dict

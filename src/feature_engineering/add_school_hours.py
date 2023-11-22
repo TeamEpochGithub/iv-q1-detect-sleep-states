ee4084b85937b9890a3bf3e48b3c758c604c2653
Jeffrey Lim <Jeffrey-Lim@outlook.com>
2023-11-22 10:32:25 +0100

    Fix school hour bugs

--- src/feature_engineering/add_school_hours.py
+++ src/feature_engineering/add_school_hours.py
@@ -3,20 +3,17 @@ from typing import Final
 
 import pandas as pd
 
-from src.feature_engineering.feature_engineering import FE, FEException
-from src.logger.logger import logger
+from src.feature_engineering.feature_engineering import FE
 
 _MONDAY: Final[int] = 0
 _TUESDAY: Final[int] = 1
 _WEDNESDAY: Final[int] = 2
 _THURSDAY: Final[int] = 3
 _FRIDAY: Final[int] = 4
-_SATURDAY: Final[int] = 5
-_SUNDAY: Final[int] = 6
 
-_SCHOOL_START_TIME: Final[str] = "08:00"
-_SCHOOL_END_TIME: Final[str] = "15:45"
-_HIGH_SCHOOL_END_TIME_MON_TUE: Final[str] = "14:20"
+_SCHOOL_START_TIME: Final[tuple[int, int]] = 8, 0
+_SCHOOL_END_TIME: Final[tuple[int, int]] = 15, 45
+_HIGH_SCHOOL_END_TIME_MON_TUE: Final[tuple[int, int]] = 16, 20
 
 
 @dataclass
@@ -24,7 +21,7 @@ class AddSchoolHours(FE):
     """Add school hours to the data.
 
     School hours in New York are 8:00-15:45 for middle school.
-    For high schools, it's 8:00-15:45 on Wednesday, Thursday, and Friday's, and 8:00-14:20 on Monday and Tuesday.
+    For high schools, it's 8:00-15:45 on Wednesday, Thursday, and Friday's, and 8:00-16:20 on Monday and Tuesday.
 
     [Source for the school times](https://www.uft.org/your-rights/know-your-rights/length-school-day)
     """
@@ -32,35 +29,46 @@ class AddSchoolHours(FE):
     def feature_engineering(self, data: pd.DataFrame) -> pd.DataFrame:
         """Add the school hour column.
 
-        The values are 0 for no school, 1 for school, 2 for middle school only.
+        The values are 0 for no school, 1 for school, 2 for high school only.
 
-        :param data: the original data
+        :param data: the original data with at least the weekday, hour, and minute column
         :return: the data with the school hour column added
         """
-        # Check if the f_weekday column exists
-        if "f_weekday" not in data.columns:
-            logger.critical("The f_weekday column is required for AddSchoolHours")
-            raise FEException("The f_weekday column is required for AddSchoolHours")
+        # Check if the f_weekday, f_hour, and f_minute column exists
+        assert {"f_weekday", "f_hour", "f_minute"}.issubset(set(data.columns)), \
+            "Missing necessary Time columns (weekday, hour, minute) for AddSchoolHours"
+
+        _num_columns_before: int = len(data.columns)  # For assertion
 
         # Create a column for the school hour
-        data["school_hour"] = 0
+        data["f_school_hour"] = 0
 
-        # Set the school hour to 2 for all timestamps between 8:00-15:45 on work days
-        middle_school_hours = (data["f_weekday"].isin(range(_MONDAY, _FRIDAY)) & (
-                data["timestamp"].dt.strftime("%H:%M") >= _SCHOOL_START_TIME) & (
-                                       data["timestamp"].dt.strftime("%H:%M") <= _SCHOOL_END_TIME))
-        data.loc[middle_school_hours, "school_hour"] = 2
+        # Check if it's (middle) school hours
+        school_hours = (
+                data["f_weekday"].isin(range(_MONDAY, _FRIDAY))
+                & ((data["f_hour"] > _SCHOOL_START_TIME[0])  # After 8
+                   | ((data["f_hour"] == _SCHOOL_START_TIME[0])
+                      & (data["f_minute"] >= _SCHOOL_START_TIME[1])))  # After 08:00
+                & ((data["f_hour"] < _SCHOOL_END_TIME[0])  # Before 15
+                   | ((data["f_hour"] == _SCHOOL_END_TIME[0])
+                      & (data["f_minute"] <= _SCHOOL_END_TIME[1])))  # Before 15:45
+        )
+        data.loc[school_hours, "f_school_hour"] = 1
 
-        # Set the school hour to 1 for all timestamps between 8:00-15:45 on Wednesday, Thursday, and Friday's and 8:00-14:20 on Monday and Tuesday
-        high_school_hours_mon_tue = (data["f_weekday"].isin([_MONDAY, _TUESDAY])) & (
-                    data["timestamp"].dt.strftime("%H:%M") >= _SCHOOL_START_TIME) & (
-                                                data["timestamp"].dt.strftime("%H:%M") <= _HIGH_SCHOOL_END_TIME_MON_TUE)
-        high_school_hours_wed_thu_fri = (
-                data["f_weekday"].isin(range(_WEDNESDAY, _FRIDAY)) & (
-                data["timestamp"].dt.strftime("%H:%M") >= _SCHOOL_START_TIME) & (
-                        data["timestamp"].dt.strftime("%H:%M") <= _SCHOOL_END_TIME))
-        data.loc[high_school_hours_mon_tue | high_school_hours_wed_thu_fri, "school_hour"] = 1
+        # Check if it's high school hours on Monday and Tuesday
+        high_school_hours_mon_tue = (
+                data["f_weekday"].isin([_MONDAY, _TUESDAY])
+                & ((data["f_hour"] > _SCHOOL_END_TIME[0])  # After 15
+                   | ((data["f_hour"] == _SCHOOL_END_TIME[0])
+                      & (data["f_minute"] > _SCHOOL_END_TIME[1])))  # After 15:45
+                & ((data["f_hour"] < _HIGH_SCHOOL_END_TIME_MON_TUE[0])  # Before 16
+                   | ((data["f_hour"] == _HIGH_SCHOOL_END_TIME_MON_TUE[0])
+                      & (data["f_minute"] <= _HIGH_SCHOOL_END_TIME_MON_TUE[1])))  # Before 16:20
+        )
+        data.loc[high_school_hours_mon_tue, "f_school_hour"] = 2
 
         # Convert the school hour column to uint8
-        data["school_hour"] = data["school_hour"].astype("uint8")
+        data["f_school_hour"] = data["f_school_hour"].astype("uint8")
+
+        assert len(data.columns) == _num_columns_before + 1, "Column f_school_hour was not added!"
         return data
--- test/fe_tests/test_add_school_hours.py
+++ test/fe_tests/test_add_school_hours.py
@@ -1,21 +1,29 @@
-# from unittest import TestCase
-#
-# import pandas as pd
-#
-# from src.feature_engineering.add_school_hours import AddSchoolHours
-#
-#
-# class TestAddSchoolHours(TestCase):
-#     def test_feature_engineering(self):
-#         fe: AddSchoolHours = AddSchoolHours()
-#
-#         data = pd.DataFrame({
-#             "timestamp": pd.date_range("2020-01-01", "2020-01-04", freq="H"),
-#         })
-#
-#         data = fe.feature_engineering(data)
-#
-#         self.assertEqual(0, data["school_hour"].iloc[0])
-#         self.assertEqual(1, data["school_hour"].iloc[8])
-#         self.assertEqual(1, data["school_hour"].iloc[10])
-#         self.assertEqual(0, data["school_hour"].iloc[18])
+from unittest import TestCase
+
+import pandas as pd
+
+from src.feature_engineering.add_school_hours import AddSchoolHours
+
+
+class TestAddSchoolHours(TestCase):
+    def test_feature_engineering(self):
+        fe: AddSchoolHours = AddSchoolHours()
+
+        data = pd.DataFrame({
+            "f_weekday": [0, 0, 0, 0, 0, 0, 3, 5, 6, 6],
+            "f_hour": [7, 8, 15, 16, 16, 16, 15, 15, 9, 0],
+            "f_minute": [59, 45, 46, 20, 21, 45, 20, 0, 59, 0],
+        })
+
+        data = fe.feature_engineering(data)
+
+        self.assertEqual(0, data["f_school_hour"].iloc[0])
+        self.assertEqual(1, data["f_school_hour"].iloc[1])
+        self.assertEqual(2, data["f_school_hour"].iloc[2])
+        self.assertEqual(2, data["f_school_hour"].iloc[3])
+        self.assertEqual(0, data["f_school_hour"].iloc[4])
+        self.assertEqual(0, data["f_school_hour"].iloc[5])
+        self.assertEqual(1, data["f_school_hour"].iloc[6])
+        self.assertEqual(0, data["f_school_hour"].iloc[7])
+        self.assertEqual(0, data["f_school_hour"].iloc[8])
+        self.assertEqual(0, data["f_school_hour"].iloc[9])
